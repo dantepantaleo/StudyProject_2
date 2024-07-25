0e85458bcd28f543effcf348346aab7a6338ca3f
dantepantaleo <dantepantaleo@gmail.com>
2024-07-25 17:09:55 -0300

    My Second Study Project

---
+++ index.js
@@ -0,0 +1,15 @@
+// Create a function that takes as parameters the number of wins and losses of a player, then returns the result to a variable.
+// Use Variables, Operators, Loops, Decision Structures and Functions
+// The ranked match balance should be calculated as (wins - losses).
+
+// If wins are less than 10 = Iron
+// If wins are between 11 and 20 = Bronze
+// If wins are between 21 and 50 = Silver
+// If wins are between 51 and 80 = Gold
+// If wins are between 81 and 90 = Diamond
+// If wins are between 91 and 100 = Legendary
+// If wins are greater than 100 and looses higher than 0 = Immortal
+// If wins are greater than 100 and no looses = Master Higlander
+
+// At the end, a message should be displayed:
+// "The Hero has a balance of {balanceWins} wins and is at the {level} level"
